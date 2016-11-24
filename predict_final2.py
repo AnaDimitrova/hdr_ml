aea4659174ab055a272f3007f012d5d6ee2c4151
Ana Dimitrova <nana@student.ethz.ch>
2016-11-24 21:40:52 +0100

    Created zip with the project that will be send to teachers

--- predict_final2.py
+++ predict_final2.py
@@ -21,17 +21,17 @@ import ProcessFiles as sp
 # Note: Preprocessing the features is actually a slow #
 # process so be mindful when you want to do that!	  #
 #######################################################
-Preprocess_Train_Files = False
-Preprocess_Train_Features = False
-Preprocess_Test_Files = False
-Preprocess_Test_Features = False
+Preprocess_Train_Files = True
+Preprocess_Train_Features = True
+Preprocess_Test_Files = True
+Preprocess_Test_Features = True
 #######################################################
 
 # LEARN  
-print("1. Starting reading train input data...")
+print("1. Starting reading train input data.")
 sp.ProcessFiles(const.Train_Data_Path, const.Precomputed_Train_Directory, Preprocess_Train_Files);
 
-print("2. Read or find features...")
+print("2. Read or find features.")
 bins = sp.BuildHistogramBins();
 input = sp.ExtractFeaturesFromAllFiles(const.Precomputed_Train_Directory, const.Preprocessed_Train_Features_File, bins, Preprocess_Train_Features)
 
@@ -39,34 +39,32 @@ input = sp.ExtractFeaturesFromAllFiles(const.Precomputed_Train_Directory, const.
 output = np.genfromtxt(const.Train_Target_File_Path, delimiter='\n')
 
 # Pick the best k features and adjust model
-print("3. Selecting best features...")
+print("3. Selecting best features.")
 features = SelectKBest(f_regression, k=const.Number_Of_Features_Project2).fit(input, output);
 
-print("4. Transforming input to selected features...")
+print("4. Transforming input to selected features.")
 input = features.transform(input)
 
-print("5. Fitting model...")
+print("5. Fitting model.")
 # Fit the linear mode with ridge regression including cross-validation with 20 folds
 reg = LassoCV(normalize=True, max_iter=10000, cv=20, n_alphas=10000)
 reg.fit(input, output)
-
-print("Model fitted")
 
 # Check score
 # Score doesn't really mean much at the moment.
 print("Score: ", log_loss(output, reg.predict(input)))
 
 # Read test input data
-print("6. Starting reading test input data...")
+print("6. Starting reading test input data.")
 sp.ProcessFiles(const.Test_Data_Path, const.Precomputed_Test_Directory, Preprocess_Test_Files)
 
-print("7. Starting processing features for test input data...")
+print("7. Starting processing features for test input data.")
 testInput = sp.ExtractFeaturesFromAllFiles(const.Precomputed_Test_Directory, const.Preprocessed_Test_Features_File, bins, Preprocess_Test_Features)
 
-print("8. Transform test input to best features...")
+print("8. Transform test input to best features.")
 testInputTransformed = features.transform(testInput)
 
-print("9. Starting predicting test data...")
+print("9. Starting predicting test data.")
 predictions = reg.predict(testInputTransformed)
 predictions = np.maximum(predictions, const.RESULT_MIN_VALUE)
 predictions = np.minimum(predictions, const.RESULT_MAX_VALUE)
@@ -75,15 +73,6 @@ predictions = np.minimum(predictions, const.RESULT_MAX_VALUE)
 predictions = np.c_[np.arange(1,const.TEST_SAMPLES + 1), predictions]
 np.savetxt(const.Test_Target_File_Path, predictions, delimiter=",", fmt="%i,%f", header="ID,Prediction", comments="")
 print ("10. Result printed at '%s'." %(const.Test_Target_File_Path))
-
-
-
-
-
-
-
-
-
 
 
 
